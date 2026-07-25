c52c04d9dedb3b422b78ae47f3bd6f81733b4e4b
aYu4ko <yimbilhssssm@gmail.com>
2026-07-26 05:06:23 +0530

    initial release

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TelemetryMetrics from './components/TelemetryMetrics';
+import './styles/dashboard.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TelemetryMetrics />
+    </div>
+  );
+}
+
+export default App;
