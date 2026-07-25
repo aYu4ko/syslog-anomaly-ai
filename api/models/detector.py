1fe6c480fab687233cd7349b8fa9447d543723a5
aYu4ko <yimbilhssssm@gmail.com>
2026-07-26 05:18:30 +0530

    fixing issues and adding missing files

--- api/models/detector.py
+++ api/models/detector.py
@@ -6,7 +6,7 @@ import joblib
 import os
 
 def train_model():
-    data = pd.read_csv('../datasets/training_data.csv')
+    data = pd.read_csv('../samples/training_data.csv')
     X = data.drop('label', axis=1)
     y = data['label']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
